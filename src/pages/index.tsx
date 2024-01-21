1fff6ae98685e70a403ebf8b37e6f1c97b5e3bd5
tuhin_008 <tuhin.thakur1233@gmail.com>
2024-01-21 17:08:47 +0530

    modal added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { useWeb3Modal } from "@web3modal/wagmi/react";
 import { useAccount, useDisconnect } from "wagmi";
 import { ConnectBtn } from "@/components/sub/button/ConnectBtn";
 import { Loader } from "@/components/Loader/Loader";
+import { Wallet } from "@/components/wallet/Wallet";
 
 export default function Home() {
 	const [isNetworkSwitchHighlighted, setIsNetworkSwitchHighlighted] =
@@ -89,7 +90,11 @@ export default function Home() {
 					</div>
 				</div>
 			</header>
+			
 			<main className={styles.main}>
+				<div className="fixed top-28 right-10 flex justify-end">
+					<Wallet/>
+				</div>
 				<div className={styles.wrapper}>
 				<div
   className="max-w-md mx-auto relative overflow-hidden z-10 bg-gray-800 p-8 rounded-lg shadow-md before:w-24 before:h-24 before:absolute before:bg-purple-600 before:rounded-full before:-z-10 before:blur-2xl after:w-32 after:h-32 after:absolute after:bg-sky-400 after:rounded-full after:-z-10 after:blur-xl after:top-24 after:-right-12"
