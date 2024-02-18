b4c4637a1f3abd671029965c263224fc6e8e7250
tuhin <tuhin.thakur1233@gmail.com>
2024-02-18 17:16:45 +0530

    ui changes for activtion button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -77,13 +77,13 @@ export default function CloudBoost() {
         {
           (address) ?
             <>
-              <div className="text-3xl mt-10 font-semibold text-white">Cloud Boost</div>
-              <div className={styles.wrapper}>
+              <div className="text-3xl mt-12 font-semibold text-white ">Cloud Boost</div>
+              <div className={styles.wrapper && `mb-20 md:mb-0`}>
                 <div className="w-full md:max-w-4xl mx-auto -mt-20 md:w-[30rem] relative overflow-hidden z-10
            bg-gray-800 p-8 rounded-lg shadow-md 
            before:w-24 before:h-24 before:absolute before:bg-purple-600 before:rounded-full 
            before:-z-10 before:blur-2xl after:w-32 after:h-32 after:absolute after:bg-sky-400
-            after:rounded-full after:-z-10 after:blur-xl after:top-24 after:-right-12">
+            after:rounded-full after:-z-10 after:blur-xl after:top-24 after:-right-12 ">
                   <Loader />
                 </div>
                 </div>
